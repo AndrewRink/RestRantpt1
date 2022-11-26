20d78555c4d4ae61bf4cc450baef47b8e266a046
Andrew <andrewcrink@gmail.com>
2022-11-25 20:31:46 -0500

    added some comments

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require ('dotenv').config()
 const express = require('express')
 const app = express()
-
+//imports the places router
 app.use('/places', require('./controllers/places'))
-
+//homepage 
 app.get('/', (req, res) => {
     res.send ('hello world')
 } )
 
-
+//adds a wildcard route/404 page
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
