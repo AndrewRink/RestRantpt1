cc5bed763bc164490a9df8edcf3ed4065b99c767
Andrew <andrewcrink@gmail.com>
2022-12-17 15:26:30 -0500

    added star ratings and emojis

--- controllers/places.js
+++ controllers/places.js
@@ -58,7 +58,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   db.Place.findByIdAndDelete(req.params.id)
-    .then(() => {
+    .then(place => {
         res.redirect('/places')
     })
     .catch(err => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,11 +8,29 @@ function show (data) {
         No comments yet!
       </h3>
     )
+    let rating = (
+      <h3 className='inactive'>
+        Not Rated Yet!
+      </h3>
+    )
     if (data.place.comments.length) {
+      let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = Math.round( sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐'
+      }
+      rating = (
+          <h3> 
+          {stars} stars
+          </h3>
+      )
       comments = data.place.comments.map(c => {
         return (
           <div className="border">
-            <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+            <h2 className="rant">{c.rant ? 'Rant! 👿' : 'Rave! 😃'}</h2>
             <h4>{c.content}</h4>
             <h3>
               <stong>- {c.author}</stong>
@@ -35,7 +53,8 @@ function show (data) {
           </div>
           <div>
             <h2>Rating</h2>
-            <p>Not Rated</p>
+            {rating}
+            <br />
           </div>
           <div>Comments
           {comments}
